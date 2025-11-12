08f457183d999a4ffa2975be84d0f7caaa1c1bf9
hugo vilar <hugovilar04@gmail.com>
2025-11-12 10:41:26 -0300

    FEAT(user) - adicionando interfaces para o gerenciamento de usuários e chamada http com lista de usuários

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private readonly _http = inject(HttpClient);
+
+  getUsers(): Observable<User[]> {
+    return this._http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  }
+}
