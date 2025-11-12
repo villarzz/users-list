1d4bc18aab76af4ec23193bc1e4a4b39f70f1f22
hugo vilar <hugovilar04@gmail.com>
2025-11-12 10:39:06 -0300

    FEAT(users-list) - adicionando layout e funcionalidades basicas do componente

---
+++ src/app/components/users-list/users-list.component.ts
@@ -0,0 +1,39 @@
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { User } from '../../interfaces/user.interface';
+import { UsersService } from '../../services/users.service';
+import { heroArrowDown, heroArrowUp } from '@ng-icons/heroicons/outline';
+
+@Component({
+  selector: 'app-users-list',
+  standalone: true,
+  imports: [CommonModule, AsyncPipe, NgIcon],
+  viewProviders: [provideIcons({ heroArrowDown, heroArrowUp })],
+  templateUrl: './users-list.component.html',
+  styleUrl: './users-list.component.css'
+})
+export class UsersListComponent {
+  users$: Observable<User[]> = this._usersService.getUsers();
+  reachedEnd: boolean = false;
+
+  constructor(private _usersService: UsersService) { }
+
+  scroll(element: HTMLElement, reachedEnd: boolean) {
+    if (reachedEnd) {
+      element.scrollBy({
+        top: element.scrollHeight * -1,
+        behavior: 'smooth'
+      });
+      this.reachedEnd = false;
+    } else {
+      element.scrollBy({
+        top: element.scrollHeight,
+        behavior: 'smooth'
+      });
+      this.reachedEnd = true;
+    }
+  }
+
+}
