b197c5f3c8839f255f9eb1d6429503e800a6a76a
hugo vilar <hugovilar04@gmail.com>
2025-11-13 08:52:38 -0300

    FEAT(users-list) - adicionando RouterLink ao componente de lista de usuários e altrando injeção de dependência para usar o novo inject

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,24 +1,24 @@
 import { Observable } from 'rxjs';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { User } from '../../interfaces/user.interface';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { UsersService } from '../../services/users.service';
 import { heroArrowDown, heroArrowUp } from '@ng-icons/heroicons/outline';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, NgIcon],
+  imports: [CommonModule, AsyncPipe, NgIcon, RouterLink],
   viewProviders: [provideIcons({ heroArrowDown, heroArrowUp })],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
   reachedEnd: boolean = false;
+  private readonly _usersService = inject(UsersService);
   users$: Observable<User[]> = this._usersService.getUsers();
-
-  constructor(private _usersService: UsersService) { }
 
   scroll(element: HTMLElement, reachedEnd: boolean) {
     if (reachedEnd) {
