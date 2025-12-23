e07052350602cb68fcd549814b5d25dbf4d6ad0b
hugo vilar <hugovilar04@gmail.com>
2025-12-23 10:38:01 -0300

    FEAT(todos) - implementando componente de lista de tarefas com integração ao serviço de tarefas

---
+++ src/app/components/user-area/components/todos/todos.component.ts
@@ -0,0 +1,29 @@
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ITodo } from '../../../../interfaces/todo.interface';
+import { todoService } from '../../../../services/todo.service';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent {
+  todos$!: Observable<ITodo[]>
+  private readonly _todoService = inject(todoService);
+  private readonly _activatedRoute = inject(ActivatedRoute)
+
+  set userId(value: number) {
+    this.todos$ = this._todoService.getTodosById(value);
+  }
+
+  ngOnInit() {
+    this._activatedRoute.parent?.params.subscribe(value => {
+      this.userId = value['userId']
+    })
+  }
+}
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ITodo } from '../interfaces/todo.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class todoService {
+  private readonly _http = inject(HttpClient);
+
+  getTodosById(id: number): Observable<ITodo[]> {
+    return this._http.get<ITodo[]>(`https://jsonplaceholder.typicode.com/todos?userId=${id}`);
+  }
+}
