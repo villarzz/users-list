b928d53fcf3383d2c4ebd8336215785cc1e8335d
hugo vilar <hugovilar04@gmail.com>
2025-12-23 14:28:57 -0300

    FEAT(todo-service) - adicionando método para obter álbuns por ID

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ITodo } from '../interfaces/todo.interface';
+import { IAlbum } from '../interfaces/album.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class todoService {
 
   getTodosById(id: number): Observable<ITodo[]> {
     return this._http.get<ITodo[]>(`https://jsonplaceholder.typicode.com/todos?userId=${id}`);
+  }
+
+  getAlbumsById(id: number): Observable<IAlbum[]> {
+    return this._http.get<IAlbum[]>(`https://jsonplaceholder.typicode.com/albums?userId=${id}`);
   }
 }
