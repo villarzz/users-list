0113dd1bd497fe92657f4edd67568de63981f50e
hugo vilar <hugovilar04@gmail.com>
2026-02-20 15:05:10 -0300

    feat(posts): adicionando seção de comentários no post

--- src/app/components/user-area/components/posts/posts.component.ts
+++ src/app/components/user-area/components/posts/posts.component.ts
@@ -1,21 +1,31 @@
 import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { IPost } from '../../../../interfaces/post.interface';
 import { postsService } from '../../../../services/posts.service';
+import { lucideArrowBigUpDash, lucideHeart } from '@ng-icons/lucide';
+import { IComment } from '../../../../interfaces/comments.interface';
+import { CommentsService } from '../../../../services/comments.service';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgClass, NgIcon],
+  viewProviders: [provideIcons({ lucideArrowBigUpDash, lucideHeart })],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
 export class PostsComponent {
+  selectedPostId!: number;
   posts$!: Observable<IPost[]>
+  openCommentsSection = false;
+  comments$!: Observable<IComment[]>
+
   private readonly _postsService = inject(postsService);
   private readonly _activatedRoute = inject(ActivatedRoute)
+  private readonly _commentsService = inject(CommentsService);
 
   set userId(value: number) {
     this.posts$ = this._postsService.getPostsById(value);
@@ -25,5 +35,17 @@ export class PostsComponent {
     this._activatedRoute.parent?.params.subscribe(value => {
       this.userId = value['userId']
     })
+  }
+
+  openComments(postId: number) {
+    this.selectedPostId = postId;
+    this.openCommentsSection = !this.openCommentsSection;
+    if (this.openCommentsSection) {
+      this.comments$ = this._commentsService.getCommentsById(postId);
+    }
+  }
+
+  numeroAleatorio1a100(): number {
+    return Math.floor(Math.random() * 100) + 1;
   }
 }
